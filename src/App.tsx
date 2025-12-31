8c2bfbc0c2436ffc8a64da949ff254b06899d3a5
krancheh <krancheh@gmail.com>
2025-12-31 17:49:52 +0300

    fix static upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import {LolaCatchGame} from './components/games/LolaCatchGame';
 import {PolinaRunGame} from './components/games/PolinaRunGame/PolinaRunGame';
 import {ShooterGame} from './components/games/ShooterGame';
 import {Menu} from './components/Menu';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom';
 
 export const App = () => (
     <div className="app-wrapper">
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Menu />} />
                 <Route path="/game" element={<ShooterGame />} />
                 <Route path="/lola-catch/:level" element={<LolaCatchGame />} />
                 <Route path="/polina-run/:level" element={<PolinaRunGame />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </div>
 );
